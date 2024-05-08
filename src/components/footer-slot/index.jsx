55d504f92c87435f521cd4623738508dab238662
Brian Smith <bsmith@axim.org>
2024-05-08 19:30:41 -0400

    feat: provide `FooterSlot`
    This allows consuming MFEs to utilize a standardized Frontend
    Plugin Framework `PluginSlot` for replacing the footer.

---
+++ src/components/footer-slot/index.jsx
@@ -0,0 +1,10 @@
+import { PluginSlot } from '@openedx/frontend-plugin-framework';
+import Footer from '../Footer';
+
+const FooterSlot = () => (
+  <PluginSlot id="footer_slot">
+    <Footer />
+  </PluginSlot>
+);
+
+export default FooterSlot;
